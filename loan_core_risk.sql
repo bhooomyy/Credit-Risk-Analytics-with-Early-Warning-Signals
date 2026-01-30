82f8105ade60154e5e38a774b0c54523154c1718
bhooomyy <bhattbhoomi0111@gmail.com>
2026-01-29 19:16:25 -0500

    created and updated loan_core_risk
    - default_flag = 1 for charged-off/default loans, else 0
    - risk_state buckets loans into DEFAULT, LATE, PAID, CURRENT, or OTHER
    - Enables easier credit-risk analysis, aggregation, and reporting

---
+++ loan_core_risk.sql
@@ -0,0 +1,39 @@
+select distinct loan_status from loan_core;
+
+create table loan_core_risk as 
+select lc.*, 
+    case 
+        when loan_status in (
+            'Charged Off',
+            'Default',
+            'Does not meet the credit policy. Status:Charged Off'
+        )
+        then 1
+        else 0
+    end as default_flag,
+        
+    case 
+        when loan_status in (
+            'Charged Off', 
+            'Default',
+            'Does not meet the credit policy. Status:Charged Off'
+        )then 'DEFAULT'
+        
+        when loan_status in(
+            'Late (16-30 days)',
+            'Late (31-120 days)',
+            'In Grace Period'
+        )then 'LATE'
+
+        when loan_status in(
+            'Fully Paid',
+            'Does not meet the credit policy. Status:Fully Paid'
+        )then 'PAID'
+
+        when loan_status ='Current'
+        then 'CURRENT'
+
+        else 'OTHER'
+    end as risk_state,
+issue_d as issue_month
+from loan_core lc;
